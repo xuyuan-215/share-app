592106c1a60b010f3d76ad3941956a2bd07a2b21
xuyuan <2414704205@qq.com>
2020-09-22 15:43:04 +0800

    first commit

---
+++ content-center/src/main/java/com/soft1851/share/ShareApplication.java
@@ -0,0 +1,13 @@
+package com.soft1851.share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareApplication.class, args);
+    }
+
+}
